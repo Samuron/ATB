8df821f80f2bb25f0accf4500f5952c61c2a572f
Ievgen Makukh <ievgen_makukh@epam.com>
2019-01-24 17:45:04 +0200

    Initial implementation

---
+++ ATB/ATB/ATB.cs
@@ -0,0 +1,14 @@
+﻿namespace ATB
+{
+    public class ATB
+    {
+        public Packet GetPacket()
+        {
+            return new Packet();
+        }
+    }
+
+    public class Packet
+    {
+    }
+}
